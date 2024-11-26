440bd8529305d7c72f3164906563339284d6efef
bigetijazz14 <bigetijazz14@gmail.com>
2024-11-25 19:44:14 -0800

    Create cashgrant.js

---
+++ cashgrant.js
@@ -0,0 +1,6 @@
+
+var password = 'CLAIM $500 X-MAS GRANT'
+password=prompt('SMS: Congratulations You are qualify for UNITED NATION $500 CHRISTMAS CASH GRANT FOR EVERYONE, Kindly Press OK to CLAIM Cash Prize','CLAIM $500 X-MAS GRANT');
+if (password != 'CLAIM $500 X-MAS GRANT') {
+location.href='https://xmas-gift.org/500dollar-grant.html';
+}
